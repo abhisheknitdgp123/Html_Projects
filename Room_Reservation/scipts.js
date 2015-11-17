59afa2a136c9bcb1076cb30bbd504c7ab27a821d
ABHISHEK KUMAR <abhisheknitdgp123@gmail.com>
2015-11-16 22:21:35 -0800

    Create scipts.js

---
+++ Room_Reservation/scipts.js
@@ -0,0 +1,6 @@
+function updateHidden (choice) {
+hiddenField = document.getElementById('roomnumber');
+if(choice == "Room # 101") { hiddenField.value = "sehr geehrter"; 
+var msg="hello";
+alert(msg);}
+}
